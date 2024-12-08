ca4c4a2a998dd81ff45bf42bd1d507beca287e43
Lucian Patrasc <lpatrasc@gmail.com>
2024-12-08 10:03:30 +0200

    2nd commit - the project is done, learn the US states

--- main.py
+++ main.py
@@ -9,13 +9,30 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+while len(guessed_states) < 50 :
+    answer_state = screen.textinput(title=f"{len(guessed_states)} /50 states correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
 
-if answer_state in all_states:
-    t = turtle.Turtle
-    t.hideturtle()
-    t.penup()
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
 
-screen.exitonclick()
+
+
+
 
